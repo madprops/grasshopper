71e61475469e56a6b058a76e84b8167e5a8f7e42
Auric Vente <mprops@protonmail.com>
2022-08-17 19:59:32 -0600

    Mods

--- js/main/ui.js
+++ js/main/ui.js
@@ -5,10 +5,29 @@ App.setup_ui = function () {
   if (top.scrollWidth > top.clientWidth) {
     App.el("#top_container").classList.add("top_container_column")
   }
+
+  App.el("#help_button").addEventListener("click", function () {
+    App.show_help()
+  })
 }
 
 // Set version in trademark
 App.set_version = function () {
   let manifest = browser.runtime.getManifest()
   App.el("#version").textContent = `v${manifest.version}`
+}
+
+// Show a help message
+App.show_help = function () {
+  let s = ""
+  s += "This is a tool to go back to often-used URLs quickly.\n"
+  s += "There's 2 modes, Favorites, and History.\n"
+  s += "Favorites are items you save by clicking the icons on the left.\n"
+  s += "History searches the browser history so you can save favorites.\n"
+  s += "You can use the mousewheel on the icons to change their positon.\n"
+  s += "Some settings are configurable in Configure.\n"
+  s += "History is only fetched when necessary, so it's fast.\n"
+  s = s.trim()
+
+  alert(s)
 }
