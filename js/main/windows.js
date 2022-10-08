8eae27171c62e65ce08c5d55cd350a8dc559794f
Auric Vente <mprops@protonmail.com>
2022-10-08 16:56:08 -0600

    Mods

--- js/main/windows.js
+++ js/main/windows.js
@@ -4,21 +4,35 @@ App.get_template = function (id) {
 }
 
 // Create a window
-App.create_window = function (id) {
+App.create_window = function (args) {
   let w = {}
-
   let el = App.create("div", "window_main")
-  let top = App.create("div", "window_top action unselectable")
-  top.textContent = "Go Back"
 
-  App.ev(top, "click", function () {
-    w.hide()
-  })
+  if (args.top === "panel") {
+    let top = App.create("div", "window_top action unselectable")
+    top.textContent = "Go Back"
+  
+    App.ev(top, "click", function () {
+      w.hide()
+    })
 
-  el.append(top)
+    el.append(top)
+  } else if (args.top === "x") {
+    let top = App.create("div", "window_top_2")
+    top.innerHTML = App.get_template(`${args.id}_top`)
+    let x = App.create("div", "window_x action unselectable")
+    x.textContent = "x"
+    top.append(x)
+  
+    App.ev(x, "click", function () {
+      w.hide()
+    })
+
+    el.append(top)
+  }
 
   let content = App.create("div", "window_content")
-  content.innerHTML = App.get_template(id)
+  content.innerHTML = App.get_template(args.id)
   el.append(content)
 
   w.element = el
@@ -26,7 +40,7 @@ App.create_window = function (id) {
 
   w.show = function () {
     w.element.style.display = "flex"
-    App.window_mode = id
+    App.window_mode = args.id
   }
   
   w.hide = function () {
@@ -34,12 +48,12 @@ App.create_window = function (id) {
     App.window_mode = "none"
   }
 
-  App.windows[id] = w
+  App.windows[args.id] = w
 }
 
 // Setup windows
 App.setup_windows = function () {
-  App.create_window("about")
+  App.create_window({id:"about", top: "panel"})
   let manifest = browser.runtime.getManifest()
   let s = `Grasshopper v${manifest.version}`
   App.el("#about_text").textContent = s
@@ -48,7 +62,7 @@ App.setup_windows = function () {
     App.windows["about"].show()
   })
 
-  App.create_window("closed_tabs") 
+  App.create_window({id:"closed_tabs", top: "x"}) 
   let filter = App.el("#closed_tabs_filter")
 
   App.filter_closed_tabs = App.create_debouncer(function () {
@@ -57,5 +71,5 @@ App.setup_windows = function () {
   
   App.ev(filter, "input", function () {
     App.filter_closed_tabs()
-  })  
+  })
 }
