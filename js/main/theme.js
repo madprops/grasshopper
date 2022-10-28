54673a7cd2780f36ea424c3f8b78431bdd3106d1
Auric Vente <mprops@protonmail.com>
2022-10-27 21:53:12 -0600

    Mods

--- js/main/theme.js
+++ js/main/theme.js
@@ -5,13 +5,15 @@ App.apply_theme = function () {
     App.set_css_var("background_color", "#252933")
     App.set_css_var("selected_color", "#4c5261")
     App.set_css_var("separator_color", "#677495")
-    App.set_css_var("filter_color", "#99a7d6")
+    App.set_css_var("filter_background_color", "#99a7d6")
+    App.set_css_var("filter_font_color", "black")
   } else if (App.state.theme === "light") {
     App.set_css_var("font_color", "black")
     App.set_css_var("background_color", "white")
     App.set_css_var("selected_color", "rgb(200, 200, 200)")
     App.set_css_var("separator_color", "rgb(200, 200, 200)")
-    App.set_css_var("filter_color", "rgb(200, 200, 200)")
+    App.set_css_var("filter_background_color", "rgb(200, 200, 200)")
+    App.set_css_var("filter_font_color", "black")
   }
 }
 
