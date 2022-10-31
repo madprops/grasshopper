3a721c78d1f9530ffe410324806f142920d41236
Auric Vente <mprops@protonmail.com>
2022-10-30 22:24:12 -0600

    Mods

--- js/main/theme.js
+++ js/main/theme.js
@@ -3,37 +3,29 @@ App.apply_theme = function () {
   if (App.state.theme === "light") {
     App.set_css_var("font_color", "black")
     App.set_css_var("background_color", "white")
-    App.set_css_var("selected_color", "rgb(200, 200, 200)")
-    App.set_css_var("separator_color", "rgb(200, 200, 200)")
-    App.set_css_var("filter_background_color", "rgb(200, 200, 200)")
-    App.set_css_var("filter_font_color", "black")
+    App.set_css_var("alt_color_1", "rgba(200, 200, 200, 0.7)")
+    App.set_css_var("alt_color_2", "rgb(200, 200, 200)")
   } 
   
   else if (App.state.theme === "hacker") {
     App.set_css_var("font_color", "rgb(0, 255, 0)")
     App.set_css_var("background_color", "black")
-    App.set_css_var("selected_color", "rgba(0, 255, 0, 0.2)")
-    App.set_css_var("separator_color", "rgba(0, 255, 0, 0.4)")
-    App.set_css_var("filter_background_color", "black")
-    App.set_css_var("filter_font_color", "rgb(0, 255, 0)")
+    App.set_css_var("alt_color_1", "rgba(0, 255, 0, 0.2)")
+    App.set_css_var("alt_color_2", "rgba(0, 255, 0, 0.4)")
   } 
   
   else if (App.state.theme === "freezer") {
     App.set_css_var("font_color", "white")
-    App.set_css_var("background_color", "#9792E3")
-    App.set_css_var("selected_color", "rgba(255, 255, 255, 0.25)")
-    App.set_css_var("separator_color", "rgba(255, 255, 255, 0.4)")
-    App.set_css_var("filter_background_color", "white")
-    App.set_css_var("filter_font_color", "black")
+    App.set_css_var("background_color", "rgb(151, 146, 227)")
+    App.set_css_var("alt_color_1", "rgba(255, 255, 255, 0.25)")
+    App.set_css_var("alt_color_2", "rgba(255, 255, 255, 0.4)")
   } 
 
   else {
     App.set_css_var("font_color", "white")
-    App.set_css_var("background_color", "#252933")
-    App.set_css_var("selected_color", "#4c5261")
-    App.set_css_var("separator_color", "#58678c")
-    App.set_css_var("filter_background_color", "#99a7d6")
-    App.set_css_var("filter_font_color", "black")
+    App.set_css_var("background_color", "rgb(37, 41, 51)")
+    App.set_css_var("alt_color_1", "rgba(76, 82, 97, 0.8)")
+    App.set_css_var("alt_color_2", "rgb(88, 103, 140)")
   }
 }
 
