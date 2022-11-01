eb0ab8d1b5c8b6f9b4fd301594e8c9e4a5c0a64b
Auric Vente <mprops@protonmail.com>
2022-11-01 00:10:29 -0600

    Mods

--- js/main/mouse.js
+++ js/main/mouse.js
@@ -1,142 +1,49 @@
-// Setup mouse
-App.setup_mouse = function () {
-  let tabs = App.el("#tabs_container")
+// Setup mouse for window
+App.setup_window_mouse = function (mode) {
+  let container = App.el(`#${mode}_container`)
 
-  // When the button is released
-  App.ev(tabs, "click", function (e) {
-    if (e.target.closest(".tabs_item")) {
-      let el = e.target.closest(".tabs_item")
-      let tab = App.get_item_by_id("tabs", el.dataset.id)
-      App.open_tab(tab)
+  App.ev(container, "click", function (e) {
+    if (e.target.closest(`.${mode}_item`)) {
+      let el = e.target.closest(`.${mode}_item`)
+      let item = App.get_item_by_id(mode, el.dataset.id)
+      App[`${mode}_action`](item)
     }
   })
 
-  // When tabs get hovered
-  App.ev(tabs, "mousemove", function (e) {
-    if (e.target.closest(".tabs_item")) {
-      let el = e.target.closest(".tabs_item")
-      let tab = App.get_item_by_id("tabs", el.dataset.id)
-      App.select_item("tabs", tab)
+  App.ev(container, "mousemove", function (e) {
+    if (e.target.closest(`.${mode}_item`)) {
+      let el = e.target.closest(`.${mode}_item`)
+      let item = App.get_item_by_id(mode, el.dataset.id)
+      App.select_item(mode, item)
     }
   })
 
-  // When tabs are middle clicked
-  App.ev(tabs, "auxclick", function (e) {
-    if (e.button === 1) {
-      if (e.target.closest(".tabs_item")) {
-        let el = e.target.closest(".tabs_item")
-        let tab = App.get_item_by_id("tabs", el.dataset.id)
-        App.confirm_close_tab(tab)
-      }
-    }
-  })
-
-  // On context menu action
-  App.ev(tabs, "contextmenu", function (e) {
-    if (e.target.closest(".tabs_item")) {
-      let el = e.target.closest(".tabs_item")
-      let tab = App.get_item_by_id("tabs", el.dataset.id)
-      App.show_item_menu("tabs", tab, e.clientX, e.clientY)
-      e.preventDefault()
-    }
-  })
-
-  let closed_tabs = App.el("#closed_tabs_container")
-
-  // On closed tabs click
-  App.ev(closed_tabs, "click", function (e) {
-    if (e.target.closest(".closed_tabs_item")) {
-      let el = e.target.closest(".closed_tabs_item")
-      let id = el.dataset.id
-      let tab = App.get_item_by_id("closed_tabs", id)
-      App.restore_tab(tab)
-    }
-  })
-  
-  // When closed tabs get hovered
-  App.ev(closed_tabs, "mousemove", function (e) {   
-    if (e.target.closest(".closed_tabs_item")) {
-      let el = e.target.closest(".closed_tabs_item")
-      let id = el.dataset.id
-      let tab = App.get_item_by_id("closed_tabs", id)
-      App.select_item("closed_tabs", tab)
-    }
-  })
-
-  // On context menu action
-  App.ev(closed_tabs, "contextmenu", function (e) {
-    if (e.target.closest(".closed_tabs_item")) {
-      let el = e.target.closest(".closed_tabs_item")
-      let id = el.dataset.id
-      let tab = App.get_item_by_id("closed_tabs", id)
-      App.show_item_menu("closed_tabs", tab, e.clientX, e.clientY)
-      e.preventDefault()
-    }
-  })
-
-  let history = App.el("#history_container")
-
-  // On history item click
-  App.ev(history, "click", function (e) {
-    if (e.target.closest(".history_item")) {
-      let el = e.target.closest(".history_item")
-      let id = el.dataset.id
-      let item = App.get_item_by_id("history", id)
-      App.open_history_item(item)
-    }
-  }) 
-  
-  // When history get hovered
-  App.ev(history, "mousemove", function (e) {   
-    if (e.target.closest(".history_item")) {
-      let el = e.target.closest(".history_item")
-      let id = el.dataset.id
-      let item = App.get_item_by_id("history", id)
-      App.select_item("history", item)
-    }
-  })
-
-  // On context menu action
-  App.ev(history, "contextmenu", function (e) {
-    if (e.target.closest(".history_item")) {
-      let el = e.target.closest(".history_item")
-      let id = el.dataset.id
-      let item = App.get_item_by_id("history", id)
-      App.show_item_menu("history", item, e.clientX, e.clientY)
+  App.ev(container, "contextmenu", function (e) {
+    if (e.target.closest(`.${mode}_item`)) {
+      let el = e.target.closest(`.${mode}_item`)
+      let item = App.get_item_by_id(mode, el.dataset.id)
+      App.show_item_menu(mode, item, e.clientX, e.clientY)
       e.preventDefault()
     }
   })  
 
-  let stars = App.el("#stars_container")
+  if (mode === "tabs") {
+    App.ev(container, "auxclick", function (e) {
+      if (e.button === 1) {
+        if (e.target.closest(`.${mode}_item`)) {
+          let el = e.target.closest(`.${mode}_item`)
+          let item = App.get_item_by_id(mode, el.dataset.id)
+          App.confirm_close_tab(item)
+        }
+      }
+    })
+  }  
+}
 
-  // On stars item click
-  App.ev(stars, "click", function (e) {
-    if (e.target.closest(".stars_item")) {
-      let el = e.target.closest(".stars_item")
-      let id = el.dataset.id
-      let item = App.get_item_by_id("stars", id)
-      App.open_stars_item(item)
-    }
-  })
-  
-  // When stars get hovered
-  App.ev(stars, "mousemove", function (e) {   
-    if (e.target.closest(".stars_item")) {
-      let el = e.target.closest(".stars_item")
-      let id = el.dataset.id
-      let item = App.get_item_by_id("stars", id)
-      App.select_item("stars", item)
-    }
-  })
-
-  // On context menu action
-  App.ev(stars, "contextmenu", function (e) {
-    if (e.target.closest(".stars_item")) {
-      let el = e.target.closest(".stars_item")
-      let id = el.dataset.id
-      let item = App.get_item_by_id("stars", id)
-      App.show_item_menu("stars", item, e.clientX, e.clientY)
-      e.preventDefault()
-    }
-  })   
+// Setup mouse for each window
+App.setup_mouse = function () {
+  App.setup_window_mouse("tabs")
+  App.setup_window_mouse("stars")
+  App.setup_window_mouse("closed_tabs")
+  App.setup_window_mouse("history") 
 }
