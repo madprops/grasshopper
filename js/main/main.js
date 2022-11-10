2b1355d4cf4d024ee0f7f3698a46fea995be06e2
Auric Vente <mprops@protonmail.com>
2022-11-10 02:35:27 -0600

    Mods

--- js/main/main.js
+++ js/main/main.js
@@ -1,7 +1,7 @@
 App.stor_settings_name = "settings_v1"
 App.stor_stars_name = "stars_v6"
 App.windows = {}
-App.filter_delay = 300
+App.filter_delay = 250
 App.color_delay = 150
 App.history_max_results = 1000
 App.history_max_months = 24
