5b00788fae4104ed5bcbb4941b93a0ab8e108f44
Auric Vente <mprops@protonmail.com>
2022-11-16 04:07:10 -0600

    Mods

--- js/main/items.js
+++ js/main/items.js
@@ -387,7 +387,7 @@ App.show_move_menu = async function (x, y, item) {
     if (win.id === App.window_id) {
       text = "This Window"
     } else {
-      let s = `${win.title.substring(0, 30).trim()} (ID: ${win.id})`
+      let s = `${win.title.substring(0, 25).trim()} (ID: ${win.id})`
       text = `Move to: ${s}`
     }
 
