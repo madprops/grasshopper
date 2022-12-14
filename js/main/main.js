d8060c80f5df642519d65c51fd9b3779bc64889c
Auric Vente <mprops@protonmail.com>
2022-12-13 18:02:38 -0600

    Mods

--- js/main/main.js
+++ js/main/main.js
@@ -16,7 +16,7 @@ App.stars_mode = "normal"
 App.history_mode = "normal"
 App.max_filters = 8
 App.num_background_images = 9
-App.theme_color_diff = 0.74
+App.theme_color_diff = 0.77
 
 NeedContext.min_width = "4.5rem"
 
