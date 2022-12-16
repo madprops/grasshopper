75ebf13820822dc0c0a8273e2a1faee230e51897
Auric Vente <mprops@protonmail.com>
2022-12-16 15:40:49 -0600

    Mods

--- js/main/main.js
+++ js/main/main.js
@@ -3,11 +3,12 @@ App.stor_stars_name = "stars_v6"
 App.stor_tab_state_name = "tab_state_v1"
 App.stor_sort_state_name = "sort_state_v3"
 App.stor_filters_name = "filters_v2"
+
 App.item_modes = ["tabs", "stars", "closed", "history"]
+
 App.windows = {}
 App.popups = {}
-App.filter_delay = 250
-App.color_delay = 150
+
 App.max_stars = 1000 * 3
 App.max_closed = 25
 App.history_max_results = 640
@@ -16,11 +17,14 @@ App.star_counter = 0
 App.max_filters = 8
 App.num_background_images = 9
 App.theme_color_diff = 0.77
+App.icon_size = 25
+
+App.filter_delay = 250
+App.color_delay = 150
 App.alert_autohide_delay = 1441
 App.save_filter_delay = 2000
 App.update_footer_delay = 200
 App.select_block_delay = 100
-App.icon_size = 25
 App.max_text_length = 200
 App.lock_backup_delay = 2000
 App.load_tabs_delay = 1000
