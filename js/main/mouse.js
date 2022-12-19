3f93dfb9b3d7c065a169d8db550763053958239b
Auric Vente <mprops@protonmail.com>
2022-12-18 20:46:20 -0600

    Mods

--- js/main/mouse.js
+++ js/main/mouse.js
@@ -2,48 +2,6 @@
 App.setup_window_mouse = function (mode) {
   let container = App.el(`#${mode}_container`)
 
-  App.ev(container, "mousemove", function (e) {
-    if (e.target.closest(`.${mode}_item`)) {
-      App.last_mousemove_e = e
-
-      if (App.select_blocked) {
-        return
-      }
-
-      if (e.target.closest(".selected")) {
-        return
-      }
-
-      let item = App.get_cursor_item(mode, e)
-      App.select_item(item, "none")
-    }
-  })
-
-  App.ev(container, "mouseover", function (e) {
-    if (e.target.closest(`.${mode}_item`)) {
-      if (App.selection_mouse_down) {
-        let item = App.get_cursor_item(mode, e)
-        App.highlight_range(item)
-      }
-    }
-  })
-
-  App.ev(container, "mousedown", function (e) {
-    if (e.button !== 0) {
-      return
-    }
-
-    if (e.shiftKey) {
-      App.selection_mouse_down = true
-      App.selection_moved = 0
-
-      if (e.target.closest(`.${mode}_item`)) {
-        let item = App.get_cursor_item(mode, e)
-        App.highlight_range(item)
-      }
-    }
-  })
-
   App.ev(container, "mouseup", function (e) {
     // Main click
     if (e.button === 0) {
@@ -58,15 +16,12 @@ App.setup_window_mouse = function (mode) {
           return
         }
   
-        if (e.shiftKey) {
-          return
-        }
-  
         if (e.ctrlKey) {
           App.toggle_highlight(item)
           App.last_highlight = item
         }
-  
+
+        
         else if (e.shiftKey) {
           App.highlight_range(item)
         }
@@ -94,8 +49,21 @@ App.setup_window_mouse = function (mode) {
     }
   })
 
-  App.ev(document, "mouseup", function () {
-    App.selection_mouse_down = false
+  App.ev(container, "mousemove", function (e) {
+    if (e.target.closest(`.${mode}_item`)) {
+      App.last_mousemove_e = e
+
+      if (App.select_blocked) {
+        return
+      }
+
+      if (e.target.closest(".selected")) {
+        return
+      }
+
+      let item = App.get_cursor_item(mode, e)
+      App.select_item(item, "none")
+    }
   })
 }
 
