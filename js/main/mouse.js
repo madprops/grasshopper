fb42aae0b7e4c991997651d3d65acd8aa0d0364e
Auric Vente <mprops@protonmail.com>
2022-12-22 22:39:49 -0600

    Mods

--- js/main/mouse.js
+++ js/main/mouse.js
@@ -69,3 +69,85 @@ App.get_cursor_item = function (mode, e) {
   let item = App.get_item_by_id(mode, el.dataset.id)
   return item
 }
+
+// Setup drag events
+App.setup_drag = function (mode, container) {
+  container.addEventListener("dragstart", function (e) {
+    if (App.settings.lock_drag && !e.ctrlKey) {
+      return
+    }
+
+    App.drag_y = e.clientY
+    App.drag_element = e.target.closest(".item")
+    let id = App.drag_element.dataset.id
+    App.drag_item = App.get_item_by_id(mode, id)
+    App.drag_start_index = App.get_item_element_index(mode, App.drag_element)
+    e.dataTransfer.setDragImage(new Image(), 0, 0)
+    e.dataTransfer.setData("text/plain", App.drag_item.url)
+
+    App.drag_items = []
+
+    if (App.drag_item.highlighted) {
+      for (let tab of App.get_items(mode)) {
+        if (tab.highlighted) {
+          App.drag_items.push(tab)
+        }
+      }
+    }
+    else {
+      App.drag_items.push(App.drag_item)
+    }
+
+    App.drag_els = []
+
+    for (let tab of App.drag_items) {
+      App.drag_els.push(tab.element)
+    }
+
+    App.drag_moved = false
+  })
+
+  container.addEventListener("dragend", function () {
+    if (!App.drag_moved) {
+      return
+    }
+
+    App.dehighlight(mode)
+    App.update_tab_index()
+  })
+
+  container.addEventListener("dragover", function (e) {
+    let direction = e.clientY > App.drag_y ? "down" : "up"
+    App.drag_y = e.clientY
+
+    if (e.target.closest(".item")) {
+      let el = e.target.closest(".item")
+
+      if (App.drag_els.includes(el)) {
+        e.preventDefault()
+        return false
+      }
+
+      let target = App.get_item_by_id(mode, el.dataset.id)
+
+      for (let item of App.drag_items) {
+        if ((target.pinned && !item.pinned) || (!target.pinned && item.pinned)) {
+          e.preventDefault()
+          return false
+        }
+      }
+
+      if (direction === "down") {
+        el.after(...App.drag_els)
+      }
+      else {
+        el.before(...App.drag_els)
+      }
+
+      App.drag_moved = true
+    }
+
+    e.preventDefault()
+    return false
+  })
+}
