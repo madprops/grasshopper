423308cfa4d2e9e49a7bb4ac2bec349fc7d8899a
Auric Vente <mprops@protonmail.com>
2023-05-23 07:20:52 -0600

    Filter mod

--- js/main/items.js
+++ js/main/items.js
@@ -348,7 +348,7 @@ App.do_item_filter = async (mode) => {
   }
 
   App.set_selected(mode, undefined)
-  App.select_first_item(mode)
+  App.select_first_item(mode, value === ``)
   App.update_footer_info(App.get_selected(mode))
   App.update_footer_count(mode)
 }
