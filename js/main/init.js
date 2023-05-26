0d4788625b1eea78ec945869bf3870513d1ec3f3
Auric Vente <mprops@protonmail.com>
2023-05-26 10:24:04 -0600

    App init mods

---
+++ js/main/init.js
@@ -0,0 +1,26 @@
+App.init = async () => {
+  let win = await browser.windows.getCurrent({populate: false})
+  App.window_id = win.id
+  App.stor_get_settings()
+  App.stor_get_stars()
+  App.stor_get_titles()
+  App.setup_theme()
+  App.setup_tabs()
+  App.setup_title_editor()
+  App.setup_stars()
+  App.setup_bookmarks()
+  App.setup_history()
+  App.setup_closed()
+  App.setup_settings()
+  App.setup_about()
+  App.setup_keyboard()
+  App.setup_popups()
+  App.setup_media()
+  App.setup_window()
+  App.setup_items()
+  App.setup_mouse()
+  App.show_first_window()
+  App.make_window_visible()
+}
+
+App.init()
