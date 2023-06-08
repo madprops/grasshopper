fb32b6cb9a129f68c8abeb234b81145f208fd8be
Auric Vente <mprops@protonmail.com>
2023-06-08 09:50:56 -0600

    Pinline file

---
+++ js/main/pinline.js
@@ -0,0 +1,49 @@
+App.pinline_debouncer = App.create_debouncer(() => {
+  App.do_check_pinline()
+}, App.pinline_debouncer_delay)
+
+App.check_pinline = () => {
+  App.pinline_debouncer.call()
+}
+
+App.do_check_pinline = () => {
+  App.pinline_debouncer.cancel()
+
+  if (App.window_mode !== `tabs`) {
+    return
+  }
+
+  if (!App.get_setting(`show_pinline`)) {
+    return
+  }
+
+  App.log(`Checking pinline`)
+  App.remove_pinline()
+  let last_pinned
+
+  for (let item of App.get_items(`tabs`)) {
+    if (!item.visible) {
+      continue
+    }
+
+    if (item.pinned) {
+      last_pinned = item
+    }
+    else {
+      if (!last_pinned) {
+        return
+      }
+      else {
+        let pinline = DOM.create(`div`, `pinline`)
+        last_pinned.element.after(pinline)
+        return
+      }
+    }
+  }
+}
+
+App.remove_pinline = () => {
+  for (let el of DOM.els(`.pinline`, DOM.el(`#tabs_container`))) {
+    el.classList.remove(`pinline`)
+  }
+}
--- js/main/tabs.js
+++ js/main/tabs.js
@@ -90,50 +90,6 @@ App.empty_previous_tabs = App.create_debouncer(() => {
   App.do_empty_previous_tabs()
 }, App.empty_previous_tabs_delay)
 
-App.pinline_debouncer = App.create_debouncer(() => {
-  App.do_check_pinline()
-}, App.pinline_debouncer_delay)
-
-App.check_pinline = () => {
-  App.pinline_debouncer.call()
-}
-
-App.do_check_pinline = () => {
-  App.pinline_debouncer.cancel()
-
-  if (App.window_mode !== `tabs`) {
-    return
-  }
-
-  if (!App.get_setting(`show_pinline`)) {
-    return
-  }
-
-  App.log(`Checking pinline`)
-  App.remove_pinline()
-  let last_pinned
-
-  for (let item of App.get_items(`tabs`)) {
-    if (!item.visible) {
-      continue
-    }
-
-    if (item.pinned) {
-      last_pinned = item
-    }
-    else {
-      if (!last_pinned) {
-        return
-      }
-      else {
-        let pinline = DOM.create(`div`, `pinline`)
-        last_pinned.element.after(pinline)
-        return
-      }
-    }
-  }
-}
-
 App.tabs_check = () => {
   App.check_playing()
 }
@@ -926,12 +882,6 @@ App.get_last_pin_index = () => {
   }
 
   return i
-}
-
-App.remove_pinline = () => {
-  for (let el of DOM.els(`.pinline`, DOM.el(`#tabs_container`))) {
-    el.classList.remove(`pinline`)
-  }
 }
 
 App.close_current_tab = async () => {
