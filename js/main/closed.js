f3e8aec545529af3ce2f0305c29f3c3b15549101
Auric Vente <mprops@protonmail.com>
2023-07-28 04:24:41 -0600

    Closed mods

--- js/main/closed.js
+++ js/main/closed.js
@@ -6,12 +6,6 @@ App.setup_closed = () => {
   ]
 
   App.setup_item_window(`closed`)
-
-  browser.sessions.onChanged.addListener(() => {
-    if (App.window_mode === `closed`) {
-      App.show_mode(`closed`)
-    }
-  })
 }
 
 App.get_closed = async () => {
