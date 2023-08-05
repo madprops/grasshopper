897a9e638a81827418eacd8c256d1df32fe31c4d
Auric Vente <mprops@protonmail.com>
2023-08-05 15:46:09 -0600

    Item hover mods

--- js/main/mouse.js
+++ js/main/mouse.js
@@ -128,16 +128,10 @@ App.mouse_over_action = (mode, e) => {
   }
 
   let item = App.get_cursor_item(mode, e)
-  item.element.classList.add(`item_hover`)
   App.update_footer_info(item)
 }
 
 App.mouse_out_action = (mode, e) => {
-  if (App.cursor_on_item(e, mode)) {
-    let item = App.get_cursor_item(mode, e)
-    item.element.classList.remove(`item_hover`)
-  }
-
   let selected = App.get_selected(mode)
 
   if (selected) {
