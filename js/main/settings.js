f694effdc7d8c39aaa93e6bfdbc5c6113013ff92
Auric Vente <mprops@protonmail.com>
2023-08-11 07:51:06 -0600

    Settings mods

--- js/main/settings.js
+++ js/main/settings.js
@@ -1082,8 +1082,10 @@ App.get_setting = (setting) => {
 }
 
 App.set_setting = (setting, value) => {
-  if (value.toString() === App.get_default_setting(setting).toString()) {
-    value = App.default_setting_string
+  if (value !== App.default_setting_string) {
+    if (value.toString() === App.get_default_setting(setting).toString()) {
+      value = App.default_setting_string
+    }
   }
 
   App.settings[setting].value = value
