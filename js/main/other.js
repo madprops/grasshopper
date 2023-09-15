2cfef008f738d8802ee4c9e87f6c238552a5bd96
Auric Vente <mprops@protonmail.com>
2023-09-14 19:35:48 -0600

    Info mod

--- js/main/other.js
+++ js/main/other.js
@@ -1,6 +1,6 @@
 App.check_first_time = () => {
   if (!App.first_time.date) {
-    App.show_alert_2(`Hi there. The main menu is the top-left button. Check the settings for some customizations`)
+    App.show_alert_2(`Hi there. The main menu is the top-left button. Check the settings for some customizations. I constantly experiment and change stuff, so expect things to break.`)
     App.first_time.date = App.now()
     App.stor_save_first_time()
   }
