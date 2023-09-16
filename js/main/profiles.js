c7a49c5649055ed836e79f1053ce3c4d99970c67
Auric Vente <mprops@protonmail.com>
2023-09-15 22:03:10 -0600

    Profile mods

--- js/main/profiles.js
+++ js/main/profiles.js
@@ -944,7 +944,7 @@ App.get_edit_items = (item) => {
   let items = []
 
   items.push({
-    icon: App.color_icon(`purple`),
+    icon: App.color_icon(`green`),
     text: `Color`,
     get_items: () => {
       return App.show_color_menu(item)
