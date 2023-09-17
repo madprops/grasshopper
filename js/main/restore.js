be3a022021cc57aed6d2539c2e51a27385aac44c
Auric Vente <mprops@protonmail.com>
2023-09-17 16:35:05 -0600

    Restore mods

---
+++ js/main/restore.js
@@ -0,0 +1,35 @@
+App.check_restore = () => {
+  if (App.get_setting(`auto_restore`) === `action`) {
+    App.restore()
+  }
+}
+
+App.start_auto_restore = () => {
+  App.clear_restore()
+  let d = App.get_setting(`auto_restore`)
+
+  if (d === `never` || d === `action`) {
+    return
+  }
+
+  let delay = App.parse_delay(d)
+
+  App.restore_timeout = setTimeout(() => {
+    App.restore()
+  }, delay)
+}
+
+App.restore = () => {
+  NeedContext.hide()
+
+  if (App.on_items()) {
+    let mode = App.active_mode
+    if ((mode !== App.primary_mode()) || App.is_filtered(mode)) {
+      App.show_primary_mode()
+    }
+  }
+}
+
+App.clear_restore = () => {
+  clearTimeout(App.restore_timeout)
+}
