a709fc85badf52d31365557b19d55284345214ea
Auric Vente <mprops@protonmail.com>
2023-09-27 16:30:46 -0600

    Start settings mod

--- js/main/app.js
+++ js/main/app.js
@@ -40,6 +40,7 @@ App.settings_addlist_ready = false
 App.backgrounds_dir = `/img/backgrounds/`
 App.num_backgrounds = 3
 App.settings_started = false
+App.settings_addlist_ready = false
 
 App.settings_save_delay = 250
 App.double_tap_delay = 300
