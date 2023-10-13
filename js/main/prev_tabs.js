0ddc98e3ea6674016038ef013231aecc81408aee
Auric Vente <mprops@protonmail.com>
2023-10-13 05:02:03 -0600

    Prev tabs mods

---
+++ js/main/prev_tabs.js
@@ -0,0 +1,74 @@
+App.empty_previous_tabs_debouncer = App.create_debouncer(() => {
+  App.do_empty_previous_tabs()
+}, App.empty_previous_tabs_delay)
+
+App.empty_previous_tabs = () => {
+  App.empty_previous_tabs_debouncer.call()
+}
+
+App.do_empty_previous_tabs = () => {
+  App.previous_tabs = []
+}
+
+App.get_previous_tabs = (include_active = false) => {
+  App.previous_tabs = App.get_items(`tabs`).slice(0)
+
+  if (!include_active) {
+    App.previous_tabs = App.previous_tabs.filter(x => !x.active)
+  }
+
+  App.previous_tabs.sort((a, b) => {
+    return a.last_accessed > b.last_accessed ? -1 : 1
+  })
+
+  App.previous_tabs_index = 0
+}
+
+App.go_to_previous_tab = () => {
+  if (!App.previous_tabs.length) {
+    App.get_previous_tabs()
+  }
+
+  App.empty_previous_tabs()
+
+  if (App.previous_tabs.length <= 1) {
+    return
+  }
+
+  let prev_tab = App.previous_tabs[App.previous_tabs_index]
+  let item = App.get_item_by_id(`tabs`, prev_tab.id)
+
+  if (item) {
+    App.focus_tab({item: item, scroll: `center`, method: `previous`})
+    App.previous_tabs_index += 1
+
+    if (App.previous_tabs_index > (App.previous_tabs.length - 1)) {
+      App.previous_tabs_index = 0
+    }
+  }
+}
+
+App.prev_tabs = (e) => {
+  let items = []
+  App.get_previous_tabs(true)
+  let max = App.get_setting(`max_prev_tabs`)
+  let playing = App.get_setting(`playing_icon`)
+
+  for (let item of App.previous_tabs.slice(0, max)) {
+    let title = App.get_title(item)
+
+    if (item.audible) {
+      title = `${playing} ${title}`
+    }
+
+    items.push({
+      image: item.favicon,
+      text: title,
+      action: () => {
+        App.focus_tab({item: item, scroll: `nearest`, show_tabs: true})
+      },
+    })
+  }
+
+  App.show_center_context(items, e)
+}
--- js/main/tabs.js
+++ js/main/tabs.js
@@ -692,56 +692,6 @@ App.detach_tabs = async (item) => {
   }
 }
 
-App.empty_previous_tabs_debouncer = App.create_debouncer(() => {
-  App.do_empty_previous_tabs()
-}, App.empty_previous_tabs_delay)
-
-App.empty_previous_tabs = () => {
-  App.empty_previous_tabs_debouncer.call()
-}
-
-App.do_empty_previous_tabs = () => {
-  App.previous_tabs = []
-}
-
-App.get_previous_tabs = (include_active = false) => {
-  App.previous_tabs = App.get_items(`tabs`).slice(0)
-
-  if (!include_active) {
-    App.previous_tabs = App.previous_tabs.filter(x => !x.active)
-  }
-
-  App.previous_tabs.sort((a, b) => {
-    return a.last_accessed > b.last_accessed ? -1 : 1
-  })
-
-  App.previous_tabs_index = 0
-}
-
-App.go_to_previous_tab = () => {
-  if (!App.previous_tabs.length) {
-    App.get_previous_tabs()
-  }
-
-  App.empty_previous_tabs()
-
-  if (App.previous_tabs.length <= 1) {
-    return
-  }
-
-  let prev_tab = App.previous_tabs[App.previous_tabs_index]
-  let item = App.get_item_by_id(`tabs`, prev_tab.id)
-
-  if (item) {
-    App.focus_tab({item: item, scroll: `center`, method: `previous`})
-    App.previous_tabs_index += 1
-
-    if (App.previous_tabs_index > (App.previous_tabs.length - 1)) {
-      App.previous_tabs_index = 0
-    }
-  }
-}
-
 App.get_active_tab = async () => {
   try {
     let tabs = await browser.tabs.query({active: true, currentWindow: true})
@@ -1098,22 +1048,4 @@ App.load_tabs = (item) => {
       App.focus_tab({item: it, scroll: `none`, method: `load`})
     }
   }, undefined, force)
-}
-
-App.prev_tabs = (e) => {
-  let items = []
-  App.get_previous_tabs(true)
-  let max = App.get_setting(`max_prev_tabs`)
-
-  for (let item of App.previous_tabs.slice(0, max)) {
-    items.push({
-      image: item.favicon,
-      text: App.get_title(item),
-      action: () => {
-        App.focus_tab({item: item, scroll: `nearest`, show_tabs: true})
-      },
-    })
-  }
-
-  App.show_center_context(items, e)
 }
