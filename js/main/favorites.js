4e3694808afdca1c750a50d1a74bfb072ff0a8ce
Auric Vente <mprops@protonmail.com>
2023-10-15 08:43:47 -0600

    Favorites

--- js/main/favorites.js
+++ js/main/favorites.js
@@ -12,12 +12,19 @@ App.fill_favorites = (mode) => {
     let cmd = App.get_command(fav.cmd)
 
     if (cmd) {
-      let btn = DOM.create(`div`, `favorite_item button`)
-      btn.textContent = cmd.icon
+      let btn = DOM.create(`div`, `favorites_item button`)
+      let icon = DOM.create(`div`, `favorites_icon`)
+      icon.append(cmd.icon)
+      btn.append(icon)
       btn.title = cmd.name
 
       DOM.ev(btn, `click`, () => {
         App.run_command(cmd)
+      })
+
+      DOM.ev(btn, `contextmenu`, (e) => {
+        e.preventDefault()
+        App.show_settings_category(`favorites`)
       })
 
       c.append(btn)
