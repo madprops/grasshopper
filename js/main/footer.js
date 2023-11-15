e3f646eca0666571b183bab02d5a162fb7dfef64
Auric Vente <mprops@protonmail.com>
2023-11-14 18:18:31 -0600

    Mods

--- js/main/footer.js
+++ js/main/footer.js
@@ -59,7 +59,7 @@ App.get_footer = (mode) => {
 }
 
 App.create_footer = (mode) => {
-  let footer = DOM.create(`div`, `footer`, `${mode}_footer`)
+  let footer = DOM.create(`div`, `footer glow`, `${mode}_footer`)
 
   let footer_count = DOM.create(`div`, `footer_count`, `${mode}_footer_count`)
   footer.append(footer_count)
