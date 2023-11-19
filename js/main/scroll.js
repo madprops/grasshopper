786ce001fbeeea631d4ea504e8dcab3f5b12295c
Auric Vente <mprops@protonmail.com>
2023-11-18 19:48:01 -0600

    Scroll mods

--- js/main/scroll.js
+++ js/main/scroll.js
@@ -48,8 +48,15 @@ App.scroll_to_item = (args = {}) => {
     if (behavior === `instant`) {
       App.do_check_scroller(args.item.mode)
     }
-    else {
-      App.check_scroller(args.item.mode)
+    else if (behavior === `smooth`) {
+      let index = App.get_item_element_index(args.item.mode, args.item.element)
+
+      if (index === 0) {
+        App.hide_scroller(args.item.mode)
+      }
+      else {
+        App.check_scroller(args.item.mode)
+      }
     }
 
     args.item.last_scroll = Date.now()
