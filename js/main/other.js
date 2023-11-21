52113b22f82d3208f59b67d825f303dbe36fba19
Auric Vente <mprops@protonmail.com>
2023-11-21 07:06:07 -0600

    Print intro mods

--- js/main/other.js
+++ js/main/other.js
@@ -7,9 +7,11 @@ App.check_first_time = () => {
 }
 
 App.show_intro_message = () => {
-  let s = `Hi there. The main menu is the top-left button. Check the settings for some customizations.
+  let s = `Hi there
+  The main menu is the top-left button
+  Check the settings for some customizations
   I constantly experiment and change stuff, so expect things to break`
-  App.alert(App.single_space(s))
+  App.alert(App.periods(s))
 }
 
 App.restart_extension = () => {
