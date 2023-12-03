66f72ba0debc501b7da7530213de3a41869e0f23
Auric Vente <mprops@protonmail.com>
2023-12-03 01:40:29 -0600

    BG effect mods

--- js/main/setting_props.js
+++ js/main/setting_props.js
@@ -195,7 +195,7 @@ App.build_settings = () => {
     background_effect_contrast: {
       name: `BG Effect Contrast`,
       type: `number`,
-      value: 20,
+      value: 12,
       placeholder: `Number`,
       min: 0,
       max: 100,
