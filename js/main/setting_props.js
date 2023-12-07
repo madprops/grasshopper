a6bee91465a6aaae5ea6b9652d7be7951d471d3b
Auric Vente <mprops@protonmail.com>
2023-12-06 22:50:29 -0600

    Color mods

--- js/main/setting_props.js
+++ js/main/setting_props.js
@@ -1997,7 +1997,7 @@ App.build_settings = () => {
       placeholder: `Opacity`,
       min: 0,
       max: 100,
-      info: `Opacity to use on the background of colored items
+      info: `Opacity to use on the background of all colored items
       A lower number means more transparent`,
       version: 1,
     },
