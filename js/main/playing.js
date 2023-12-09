47483115e1d7601124a6bf04ccc0d62a6f5ab31b
Auric Vente <mprops@protonmail.com>
2023-12-08 22:53:37 -0600

    Mods

--- js/main/playing.js
+++ js/main/playing.js
@@ -1,6 +1,6 @@
 App.create_playing_icon = (mode) => {
   btn = DOM.create(`div`, `button icon_button playing_icon hidden`, `playing_icon_${mode}`)
-  btn.title = `Go To Playing Tab (Ctrl + Dot) - Right Click to show playing tabs`
+  btn.title = `Go To Playing Tab (Ctrl + Dot) - Right Click to show Playing Tabs`
   let icon = App.get_svg_icon(`speaker`)
 
   DOM.ev(btn, `click`, () => {
