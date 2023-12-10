c6a6fea4759a6f2468a4e1bbec4848d4d53739fd
Auric Vente <mprops@protonmail.com>
2023-12-10 01:25:16 -0600

    Zip mods

--- utils/zip.rb
+++ utils/zip.rb
@@ -1,9 +1,22 @@
 #!/usr/bin/env ruby
 require "json"
+require "fileutils"
+
+# Read the manifest
 file = File.read("manifest.json")
 manifest = JSON.parse(file)
 version = manifest["version"].gsub(".", "_")
 name = manifest["name"].downcase.split.join("_")
-zip_name = "#{name}_v#{version}.zip"
-`zip #{zip_name} -x \*.zip -r \*`
-puts "Created #{zip_name}"
+
+# Delete the old zip file
+old_name = Dir.glob("#{name}*.zip").first
+
+if File.exist?(old_name)
+	File.delete(old_name)
+	puts "Removed #{old_name}"
+end
+
+# Create the new zip file
+new_name = "#{name}_v#{version}.zip"
+`zip #{new_name} -x \*.zip -r \*`
+puts "Created #{new_name}"
