6c9bde8f6fc31e406071eb9b1d0491713bbc7260
Auric Vente <mprops@protonmail.com>
2024-02-06 15:26:58 -0600

    Fixes

--- js/main/utils.js
+++ js/main/utils.js
@@ -532,14 +532,9 @@ App.some = (value) => {
 }
 
 App.remove_undefined = (arr) => {
-  let i = 0
-
-  while (i < arr.length) {
+  for (let i = arr.length - 1; i >= 0; i--) {
     if (arr[i] === undefined) {
       arr.splice(i, 1)
-    }
-    else {
-      i++
     }
   }
 }
