ce65b7327ff3d5cc6dbb605d028eaa07e08595de
Auric Vente <mprops@protonmail.com>
2024-06-06 23:40:09 -0600

    Lint mods

--- eslint.config.js
+++ eslint.config.js
@@ -6,20 +6,5 @@ module.exports = [
       "quotes": ["error", "backtick"],
       "no-console": "error",
     },
-    languageOptions: {
-      globals: {
-        App: "writable",
-        DOM: "writable",
-        Addlist: "writable",
-        NiceGesture: "writable",
-        NeedContext: "writable",
-        Menubutton: "writable",
-        ColorLib: "writable",
-        AColorPicker: "writable",
-        dateFormat: "writable",
-        jdenticon: "writable",
-        browser: "writable",
-      }
-    }
   }
 ]
