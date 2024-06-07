2a54a3d7537e9d4d50fe5dd47161baa8ee8cb19a
Auric Vente <mprops@protonmail.com>
2024-06-07 00:07:49 -0600

    Lint mods

---
+++ eslint.config.mjs
@@ -0,0 +1,34 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+export default [
+  pluginJs.configs.recommended,
+
+  {languageOptions: {globals: globals.browser}},
+
+  {
+    rules: {
+      "semi": "off",
+      "no-unused-vars": "off",
+      "indent": ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      "quotes": ["error", "backtick"],
+      "no-console": "error",
+    },
+    languageOptions: {
+      globals: {
+        App: "writable",
+        DOM: "writable",
+        Addlist: "writable",
+        NiceGesture: "writable",
+        NeedContext: "writable",
+        Menubutton: "writable",
+        ColorLib: "writable",
+        AColorPicker: "writable",
+        dateFormat: "writable",
+        jdenticon: "writable",
+        browser: "writable",
+      }
+    }
+  }
+]
---
+++ utils/check.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+eslint -c eslint.config.mjs js/main/*.js
