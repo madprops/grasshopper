302e391313e9dc6e50bc2650f3256820f695739c
Auric Vente <mprops@protonmail.com>
2024-06-15 17:23:10 -0600

    Mods

--- js/main/main_title.js
+++ js/main/main_title.js
@@ -6,6 +6,9 @@ App.create_main_title = (mode) => {
   let title = App.get_setting(`main_title`)
   inner.textContent = title
 
+  App.trigger_title(el, `middle_click_main_title`)
+  App.trigger_title(el, `double_click_main_title`)
+
   DOM.ev(el, `contextmenu`, (e) => {
     App.main_title_right_click(e)
   })
