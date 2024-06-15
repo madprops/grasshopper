a6ecbf256b4e7cdcb30da818c71eecca8d8cbaa7
Auric Vente <mprops@protonmail.com>
2024-06-15 15:39:54 -0600

    Main Title mods

--- js/main/setting_props.js
+++ js/main/setting_props.js
@@ -758,6 +758,7 @@ App.build_settings = () => {
       value: [
         {cmd: `edit_main_title`},
         {cmd: `edit_global_notes`},
+        {cmd: `show_theme_menu`},
       ],
       info: `Menu to show when clicking the Main Title`,
       version: 1,
