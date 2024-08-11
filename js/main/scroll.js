8695e5be07cf7f6d4c1183a7df081386f95a8e81
Auric Vente <mprops@protonmail.com>
2024-08-11 06:44:32 -0600

    Command info mods

--- js/main/scroll.js
+++ js/main/scroll.js
@@ -168,10 +168,10 @@ App.do_check_scroller = (mode = App.active_mode) => {
 App.create_scroller = (mode) => {
   let scroller = DOM.create(`div`, `scroller glowbox`, `${mode}_scroller`)
   let content = DOM.create(`div`, `scroller_content`)
-  let top = App.get_cmd_name(`go_to_top`)
-  let scroll_up = App.get_cmd_name(`scroll_up`)
-  let page_up = App.get_cmd_name(`page_up`)
-  scroller.title = `This is the Scroller\nClick: ${top}\nRight Click: ${scroll_up}\nMiddle Click: ${page_up}`
+  let click = App.get_cmd_name(`go_to_top`)
+  let rclick = App.get_cmd_name(`scroll_up`)
+  let mclick = App.get_cmd_name(`page_up`)
+  scroller.title = `This is the Scroller\nClick: ${click}\nRight Click: ${rclick}\nMiddle Click: ${mclick}`
   let text = DOM.create(`div`)
   text.textContent = `Go To Top`
   let percentage = DOM.create(`div`, ``, `${mode}_scroller_percentage`)
