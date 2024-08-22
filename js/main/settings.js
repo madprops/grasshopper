af36b735cb22c8ec691cbadbad7be75de1ee8666
Auric Vente <mprops@protonmail.com>
2024-08-21 22:06:53 -0600

    Mods

--- js/main/settings.js
+++ js/main/settings.js
@@ -1513,7 +1513,4 @@ App.madprops_settings = () => {
   App.set_theme(3)
   App.set_setting({setting: `load_lock`, value: true, action: true})
   App.set_setting({setting: `autohide_context`, value: true, action: true})
-  App.set_setting({setting: `url_one`, value: `https://lab.serotoninphobia.info`, action: true})
-  App.set_setting({setting: `url_two`, value: `https://arngren.net/`, action: true})
-  App.set_setting({setting: `url_three`, value: `https://www.youtube.com/watch?v=spdfnqS3bDg`, action: true})
 }
