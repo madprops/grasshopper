e1083dbdd09929b0ceb15dc5147a027d733ad6c7
Auric Vente <mprops@protonmail.com>
2024-09-05 09:07:39 -0600

    Mods

--- js/main/users.js
+++ js/main/users.js
@@ -10,25 +10,27 @@ App.user_settings = (who) => {
 App.user_madprops_settings = () => {
   App.set_theme(3)
 
-  App.set_setting({ setting: `load_lock`, value: true })
-  App.set_setting({ setting: `autohide_context`, value: true })
-  App.set_setting({ setting: `double_click_favorites_top`, value: `toggle_taglist` })
-  App.set_setting({ setting: `middle_click_favorites_top`, value: `toggle_pin_tabs` })
+  App.set_setting({setting: `load_lock`, value: true})
+  App.set_setting({setting: `autohide_context`, value: true})
+  App.set_setting({setting: `max_recent_tabs`, value: 20})
 
-  App.set_setting({ setting: `wheel_up_favorites_top`, value: `jump_tabs_pin_up` })
-  App.set_setting({ setting: `wheel_down_favorites_top`, value: `jump_tabs_pin_down` })
+  App.set_setting({setting: `double_click_favorites_top`, value: `toggle_taglist`})
+  App.set_setting({setting: `middle_click_favorites_top`, value: `toggle_pin_tabs`})
 
-  App.set_setting({ setting: `wheel_up_favorites_center`, value: `jump_tabs_all_up` })
-  App.set_setting({ setting: `wheel_down_favorites_center`, value: `jump_tabs_all_down` })
+  App.set_setting({setting: `wheel_up_favorites_top`, value: `jump_tabs_pin_up`})
+  App.set_setting({setting: `wheel_down_favorites_top`, value: `jump_tabs_pin_down`})
 
-  App.set_setting({ setting: `wheel_up_favorites_bottom`, value: `jump_tabs_normal_up` })
-  App.set_setting({ setting: `wheel_down_favorites_bottom`, value: `jump_tabs_normal_down` })
+  App.set_setting({setting: `wheel_up_favorites_center`, value: `jump_tabs_all_up`})
+  App.set_setting({setting: `wheel_down_favorites_center`, value: `jump_tabs_all_down`})
 
-  App.set_setting({ setting: `wheel_up_footer`, value: `jump_tabs_header_up` })
-  App.set_setting({ setting: `wheel_down_footer`, value: `jump_tabs_header_down` })
-  App.set_setting({ setting: `extra_menu_mode`, value: `flat` })
+  App.set_setting({setting: `wheel_up_favorites_bottom`, value: `jump_tabs_normal_up`})
+  App.set_setting({setting: `wheel_down_favorites_bottom`, value: `jump_tabs_normal_down`})
 
-  let cmd = { cmd: `locust_swarm`, alt: `generate_password` }
+  App.set_setting({setting: `wheel_up_footer`, value: `jump_tabs_header_up`})
+  App.set_setting({setting: `wheel_down_footer`, value: `jump_tabs_header_down`})
+  App.set_setting({setting: `extra_menu_mode`, value: `flat`})
+
+  let cmd = {cmd: `locust_swarm`, alt: `generate_password`}
   App.prepend_list_setting(`favorites_menu`, cmd)
 
   let urls = [
