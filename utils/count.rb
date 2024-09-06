5780dd03824cb878e69d17a77ff3e8869ec72404
Auric Vente <mprops@protonmail.com>
2024-09-05 19:04:15 -0600

    Count

--- utils/count.rb
+++ utils/count.rb
@@ -1,22 +1,26 @@
 #!/usr/bin/env ruby
 
 # Get total line numbers of a file
-def get_total_lines(file_path)
+def get_lines(file_path)
   total_lines = 0
+
   File.open(file_path, "r") do |file|
     total_lines += file.readlines.size
   end
+
   total_lines
 end
 
 # Change directory to subdir and get total line numbers of all files
-def get_total_lines_in_subdir(subdir_path)
+def subdir_lines(subdir_path, ext)
   total_lines = 0
+
   Dir.chdir(subdir_path) do
-    Dir.glob("*").each do |file|
-      total_lines += get_total_lines(file) if File.file?(file)
+    Dir.glob("*.#{ext}").each do |file|
+      total_lines += get_lines(file) if File.file?(file)
     end
   end
+
   total_lines
 end
 
@@ -26,11 +30,11 @@ app_file = "js/app.js"
 main_file = "main.html"
 css_file = "css/style.css"
 
-lines_main_path = get_total_lines_in_subdir(main_path)
-lines_libs_path = get_total_lines_in_subdir(libs_path)
-lines_app_file = get_total_lines(app_file)
-lines_main_file = get_total_lines(main_file)
-lines_css_file = get_total_lines(css_file)
+lines_main_path = subdir_lines(main_path, "js")
+lines_libs_path = subdir_lines(libs_path, "js")
+lines_app_file = get_lines(app_file)
+lines_main_file = get_lines(main_file)
+lines_css_file = get_lines(css_file)
 
 puts "#{main_path}: #{lines_main_path}"
 puts "#{libs_path}: #{lines_libs_path}"
@@ -40,6 +44,6 @@ puts "#{css_file}: #{lines_css_file}"
 
 # Sum all lines
 total_lines = lines_main_path + lines_libs_path +
-    lines_app_file + lines_main_file + lines_css_file
+lines_app_file + lines_main_file + lines_css_file
 
-puts "Total: #{total_lines}"
+puts "TOTAL: #{total_lines}"
