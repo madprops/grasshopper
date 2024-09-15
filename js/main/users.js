5a31a283168d7d9e224e4788ab5d5d684920b4ed
Auric Vente <mprops@protonmail.com>
2024-09-15 00:49:24 -0600

    Main title mods

--- js/main/users.js
+++ js/main/users.js
@@ -20,6 +20,9 @@ App.user_madprops_settings = () => {
   App.set_setting({setting: `main_title_signal`, value: `send_signal_sig_np`})
   App.set_setting({setting: `show_main_title`, value: true})
   App.set_setting({setting: `main_title`, value: `Loading...`})
+  App.set_setting({setting: `main_title_colors`, value: true})
+  App.set_setting({setting: `main_title_text_color`, value: `rgba(255, 255, 255, 1)`})
+  App.set_setting({setting: `main_title_background_color`, value: `rgba(60, 60, 60, 1)`})
 
   // Wheel
 
