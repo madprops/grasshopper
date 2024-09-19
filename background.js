c4660782d040ed63ae4624a951b2cbd5664d81e1
Auric Vente <mprops@protonmail.com>
2024-09-19 08:34:56 -0600

    Background mods

--- background.js
+++ background.js
@@ -1,6 +1,14 @@
-function open_popup(mode) {
-  localStorage.setItem(`init_mode`, mode)
+function open_popup() {
   browser.browserAction.openPopup()
+}
+
+function set_item(what, value) {
+  localStorage.setItem(what, value)
+}
+
+function open_popup_mode(mode) {
+  set_item(`init_mode`, mode)
+  open_popup()
 }
 
 function browser_command(num) {
@@ -8,22 +16,22 @@ function browser_command(num) {
 }
 
 async function popup_command(num) {
-  localStorage.setItem(`init_popup_command`, num)
-  browser.browserAction.openPopup()
+  set_item(`init_popup_command`, num)
+  open_popup()
 }
 
 browser.commands.onCommand.addListener((command) => {
   if (command === `popup_tabs`) {
-    open_popup(`tabs`)
+    open_popup_mode(`tabs`)
   }
   else if (command === `popup_history`) {
-    open_popup(`history`)
+    open_popup_mode(`history`)
   }
   else if (command === `popup_bookmarks`) {
-    open_popup(`bookmarks`)
+    open_popup_mode(`bookmarks`)
   }
   else if (command === `popup_closed`) {
-    open_popup(`closed`)
+    open_popup_mode(`closed`)
   }
   else if (command.startsWith(`browser_command_`)) {
     let num = command.split(`_`).at(-1)
