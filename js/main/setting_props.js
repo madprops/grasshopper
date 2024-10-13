28d67e490aa332ebd53c402d3cda52857989eb26
Auric Vente <mprops@protonmail.com>
2024-10-13 00:48:58 -0600

    Tab box mods

--- js/main/setting_props.js
+++ js/main/setting_props.js
@@ -4712,7 +4712,7 @@ App.build_settings = () => {
     max_recent_tabs: {
       name: `Max Recent Tabs`,
       type: `number`,
-      value: 16,
+      value: 15,
       placeholder: `Number`,
       min: App.number_min,
       max: App.number_max,
