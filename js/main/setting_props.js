a15689e8e2a6c6484a81ac3340805942b2c9d754
Auric Vente <mprops@protonmail.com>
2024-10-16 15:41:54 -0600

    Click press mod

--- js/main/setting_props.js
+++ js/main/setting_props.js
@@ -3179,7 +3179,7 @@ App.build_settings = () => {
     click_press_item: {
       name: `Click Press Item`,
       type: `menu`,
-      value: `none`,
+      value: `filter_domain`,
       info: `What command to run when pressing the left mouse button on an item for a short time`,
       version: 1,
       setup: (key) => {
