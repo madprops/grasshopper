7556f048f20d84e7800da6ce32c2836315a1de69
Auric Vente <mprops@protonmail.com>
2024-10-20 16:52:12 -0600

    Mouse inside mods

--- js/app.js
+++ js/app.js
@@ -181,7 +181,7 @@ App.last_settings_scroll_delay = 350
 App.default_combo_delay = 100
 App.mouse_over_delay = 150
 App.mouse_out_delay = 150
-App.mouse_inside_delay = 800
+App.mouse_inside_delay = App.SECOND
 
 App.settings_icons = {
   all: `🔍`,
