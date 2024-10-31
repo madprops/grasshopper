f3c9b60ed9b23d7f8980da5e2ce44b839a534445
Auric Vente <mprops@protonmail.com>
2024-10-31 09:07:59 -0600

    Mods

--- utils/check.sh
+++ utils/check.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
-npx eslint -c eslint.config.mjs js/main/*.js
+npx eslint --cache -c eslint.config.mjs js/main/*.js
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
-npx eslint -c eslint.config.mjs js/overrides.js
+npx eslint --cache -c eslint.config.mjs js/overrides.js
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
-npx eslint -c eslint.config.mjs js/app.js
+npx eslint --cache -c eslint.config.mjs js/app.js
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
-npx eslint -c eslint.config.mjs js/init.js
+npx eslint --cache -c eslint.config.mjs js/init.js
 if [ $? -ne 0 ]; then
   exit 1
 fi
 
-npx eslint -c eslint.config.mjs background/*.js
+npx eslint --cache -c eslint.config.mjs background/*.js
 if [ $? -ne 0 ]; then
   exit 1
 fi
