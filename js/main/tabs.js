c170224ec104ea345541267ab0f9468b6b012332
Auric Vente <mprops@protonmail.com>
2024-11-04 03:47:33 -0600

    Effect mods

--- js/main/tabs.js
+++ js/main/tabs.js
@@ -317,9 +317,6 @@ App.tabs_action = async (args = {}) => {
     else if (args.from === `tab_cmd`) {
       args.scroll = `nearest_instant`
     }
-    else if (args.from === `autoclick`) {
-      args.scroll = `none`
-    }
     else {
       args.scroll = `nearest_smooth`
     }
