92286b3b49de900a1d4eb9888a9385ae3df62cfb
Auric Vente <mprops@protonmail.com>
2024-12-01 07:39:49 -0600

    Recent tabs fix

--- js/main/tabs.js
+++ js/main/tabs.js
@@ -952,19 +952,21 @@ App.load_tabs = (item, multiple = true) => {
 }
 
 App.check_tab_first = () => {
-  if (App.tabs_recent()) {
-    if (!App.tabs_to_first.length) {
-      return
-    }
-
-    for (let tab of App.tabs_to_first) {
-      if (tab && !tab.removed) {
-        App.make_item_first(tab)
-      }
-    }
-
-    App.tabs_to_first = []
+  if (!App.tabs_recent()) {
+    return
   }
+
+  if (!App.tabs_to_first.length) {
+    return
+  }
+
+  for (let tab of App.tabs_to_first) {
+    if (tab && !tab.removed) {
+      App.make_item_first(tab)
+    }
+  }
+
+  App.tabs_to_first = []
 }
 
 App.check_on_tabs = async (show_all = true) => {
