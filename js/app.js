c41bb18016351e1a87245c8369847c02dab03ab7
Auric Vente <mprops@protonmail.com>
2025-01-06 23:22:12 -0600

    Setting guides

--- js/app.js
+++ js/app.js
@@ -540,32 +540,3 @@ App.footer_down_click_cmd = `move_tabs_to_bottom`
 App.footer_down_middle_click_cmd = `select_items_below`
 App.footer_down_shift_click_cmd = `page_down`
 App.footer_down_ctrl_click_cmd = `jump_tabs_all_down`
-
-App.setting_guides = [
-  {
-    title: `Importing settings`,
-    text: `Settings can be imported by using JSON.
-
-    You can import all settings or just some of them, the ones you need.
-
-    This means other people can share their configuration with you.
-
-    Or you can back up your settings and apply them on another computer.`,
-  },
-  {
-    title: `Type to filter lists`,
-    text: `On any list, like command lists, you can type to filter the items.
-
-    This is very important since it's much faster than scrolling down the lists to find items.`,
-  },
-  {
-    title: `Don't load unloaded tabs on click`,
-    text: `To avoid loading unloaded tabs when clicking on them:
-
-    Go to "Triggers", then change "Click Item (Tabs)" to "Soft Action".
-
-    Now to load them, double clicking is required.
-
-    Single click will only select them, not load them.`,
-  },
-]
---
+++ js/main/guides.js
@@ -0,0 +1,28 @@
+App.setting_guides = [
+  {
+    title: `Importing settings`,
+    text: `Settings can be imported by using JSON.
+
+    You can import all settings or just some of them, the ones you need.
+
+    This means other people can share their configuration with you.
+
+    Or you can back up your settings and apply them on another computer.`,
+  },
+  {
+    title: `Type to filter lists`,
+    text: `On any list, like command lists, you can type to filter the items.
+
+    This is very important since it's much faster than scrolling down the lists to find items.`,
+  },
+  {
+    title: `Don't load unloaded tabs on click`,
+    text: `To avoid loading unloaded tabs when clicking on them:
+
+    Go to "Triggers", then change "Click Item (Tabs)" to "Soft Action".
+
+    Now to load them, double clicking is required.
+
+    Single click will only select them, not load them.`,
+  },
+]
