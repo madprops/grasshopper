2c037b5d493b0c555e3ba884a1aa4a311ad59ab4
Auric Vente <mprops@protonmail.com>
2025-01-24 18:33:37 -0600

    Mods

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+// ########################################################
+// Please don't ruin the code with terrible formatting
+// There's an eslint config that should clean up things
+// It should be ran periodically to find and fix flaws
+// But apart from that, all the code is structured manually
+// ########################################################
+
 const App = {}
 
 App.stor_settings_name = `settings_v200`
