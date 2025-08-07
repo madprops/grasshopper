0c8f71df4ec756291ec3c79ce5c3a170bf44da2a
Auric Vente <mprops@protonmail.com>
2025-08-06 21:48:31 -0600

    Lockscreen words

--- utils/check.sh
+++ utils/check.sh
@@ -7,7 +7,7 @@ last_tag=$(git describe --tags --abbrev=0)
 # Pick one
 # files=$(git diff --name-only $last_tag HEAD -- '*.js')
 files=$(git ls-files -- "*.js")
-files=$(echo $files | tr " " "\n" | grep -v "/libs/" | tr "\n" " ")
+files=$(echo $files | tr " " "\n" | grep -v "/libs/" | grep -v "words.js" | tr "\n" " ")
 
 if [ -n "$files" ]; then
   npm run --silent lint $files
