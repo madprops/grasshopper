adbc5a279213b23a7f22f7e3a90d4f10821d4cc3
Auric Vente <mprops@protonmail.com>
2025-12-10 13:04:48 -0600

    Calculator

--- js/main/math.js
+++ js/main/math.js
@@ -14,3 +14,43 @@ App.math_eval = (input = ``) => {
 
   return ans
 }
+
+App.use_calculator = () => {
+  App.show_textarea({
+    title: `Calculator`,
+    text: ``,
+    monospace: true,
+    readonly: false,
+    buttons: [
+      {
+        text: `Copy`,
+        action: () => {
+          App.copy_to_clipboard(password)
+          App.close_textarea()
+        },
+      },
+      {
+        text: `Calculate`,
+        action: (text) => {
+          let ans = App.math_eval(text)
+
+          if (ans) {
+            let num = App.number_format(ans, 5)
+            App.alert(num)
+          }
+        },
+      },
+    ],
+  })
+}
+
+App.number_format = (num, decimals) => {
+  // maximumFractionDigits caps the decimals
+  // it defaults to not showing trailing zeros
+  let formatter = new Intl.NumberFormat('en-US', {
+    maximumFractionDigits: decimals,
+    useGrouping: true // Set true if you want "1,000"
+  })
+
+  return formatter.format(num)
+}
