da02b8eb5580603222d29aeeeb95ec5ffb4f786b
Auric Vente <mprops@protonmail.com>
2026-01-29 11:35:49 -0600

    Mods

--- js/main/oracle.js
+++ js/main/oracle.js
@@ -2,7 +2,7 @@ App.show_oracle = () => {
   let words = App.get_random_words(App.oracle_words)
   let emoji = App.get_random_emoji()
   words.push(emoji)
-  let text = words.join(`\n`)
+  let text = words.join(` `)
 
   App.show_textarea({
     title: `Oracle`,
