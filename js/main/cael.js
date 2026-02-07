0c3c4c5f10f67043c96a595f45f2055c04891cd2
Auric Vente <mprops@protonmail.com>
2026-02-06 20:57:20 -0600

    Mods

--- js/main/cael.js
+++ js/main/cael.js
@@ -1,3 +1,11 @@
+App.ai_config = {
+  gemini: {
+    url: "https://generativelanguage.googleapis.com/v1beta/openai/chat/completions",
+    model: "gemini-flash-latest",
+  },
+  cael_system: `Your name is Cael, an ancient grasshopper deity. I am here to ask you a question, or to entertain you for a while.`
+}
+
 App.talk_to_cael = async () => {
   App.show_textarea({
     title: `Cael`,
@@ -18,10 +26,51 @@ App.talk_to_cael = async () => {
       },
       {
         text: `Send`,
-        action: () => {
-          //
+        action: (text) => {
+          App.ask_cael(text)
         },
       },
     ],
   })
+}
+
+App.ask_cael = (text) => {
+  App.ask_ai(App.ai_config.cael_system, text)
+}
+
+App.ask_ai = async (system, prompt) => {
+  let headers = {
+    "Content-Type": `application/json`,
+  }
+
+  let body = {
+    model: App.ai_config.gemini.model,
+    stream: false,
+  }
+
+  body.messages = [
+    {role: `system`, content: system},
+    ...history,
+    {role: `user`, content: prompt},
+  ]
+
+  try {
+    let response = await fetch(App.ai_config.gemini.url, {
+      method: `POST`,
+      headers: headers,
+      body: JSON.stringify(body),
+    })
+
+    let data = await response.json()
+
+    if (!response.ok) {
+      let err = data.error?.message || JSON.stringify(data)
+      throw new Error(`AI API Error (${App.ai_source}): ${err}`)
+    }
+
+    return data.choices[0].message.content
+  }
+  catch (error) {
+    console.error(`AI Request Failed:`, error)
+  }
 }
