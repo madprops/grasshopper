a6068f32c8d45a22d7b32870019e18f47877ed56
Auric Vente <mprops@protonmail.com>
2026-03-02 14:04:47 -0600

    Mods

---
+++ utils/make_chrome.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./utils/check.sh && ./utils/prepare_chrome.sh && ./utils/zip.rb chrome
---
+++ utils/make_firefox.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+./utils/check.sh && ./utils/prepare_firefox.sh && ./utils/zip.rb firefox
